6004c5ec96514d72bdabe852a66123fd1a371eb1
bpfox77 <56087137+bpfox77@users.noreply.github.com>
2022-07-28 13:42:06 -0500

    removed images

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 function Type() {
   return (
     <Typewriter
       options={{
         strings: [
-          "Language AI",
-          "Web3",
-          "Story Science",
-          "Computer Vision",
-          "Mixed Realities",
+          'Language AI',
+          'Web3',
+          'Story Science',
+          'Computer Vision',
+          'Mixed Realities',
         ],
         autoStart: true,
         loop: true,
